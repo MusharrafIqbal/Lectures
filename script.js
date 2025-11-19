92a54f4e1334c77297a34d5eecc714c7805aa9c6
musharrafiqbal <musharrafiqbal786@gmail.com>
2025-11-20 02:57:46 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,132 @@
+// Lecture 29
+
+
+
+// let day = prompt("Enter The Day")
+
+// switch (day) {
+//     case 'a':
+//         document.writeln("Today is Monday");
+//         break
+//     case 'b':
+//         document.writeln('Today is Tuesday')
+//         break
+//     case 'c':
+//         document.writeln("Today is Wednesday");
+//         break
+//     case 'd':
+//         document.writeln('Today is Thursday')
+//         break
+//     case 'e':
+//         document.writeln("Today is Friday");
+//         break
+//     case 'f':
+//         document.writeln('Today is Saturday')
+//         break
+//     case 'g':
+//         document.writeln("Today is Sunday");
+//         break
+//     default:
+//         document.writeln("Please Enter Correct Number")
+//         break    
+// }
+
+// let num =+ prompt("Enter The Number")
+
+// let even = num%2==0
+// let odd = num%2==1
+
+
+
+// switch(even){
+//     case even:
+//         document.writeln("This is Even Number")
+//         break
+//         case odd:
+//             document.writeln("It is Odd Number")
+//             break
+// }
+
+// document.getElementById('demo').innerHTML = 'hellow world'
+// document.getElementById('headline').innerHTML = 'hellow again'
+
+
+// let num = prompt("Enter The Number")
+
+// if(num%2==0){
+
+//  document.getElementById('box').innerHTML = '<img src="https://www.shutterstock.com/image-vector/icons-even-numbers-on-white-260nw-282248162.jpg" alt="">'
+// }
+
+// else if(num%2==1){
+//     document.getElementById('box').innerHTML ='<img src="https://www.shutterstock.com/image-photo/colorful-odd-number-on-white-260nw-1602605152.jpg" alt="">'
+// }
+
+let num =prompt("enter num")
+ let text =  (num%2==0) ? "It is even": (num%2==1) ? 'it is odd' : 'Enter Correct Number'
+
+ document.writeln(text)
+
+
+//  Lecture  30
+
+// let d = new Date()
+// let d = new Date().getDay()
+// let d = new Date().getMonth() 
+// let d = new Date().getFullYear()
+// let d = new Date().getDate()
+// let d = new Date().getHours()
+// let d = new Date().getMinutes()
+// let d = new Date().getSeconds()
+// document.writeln(d)
+
+// let date = new Date()
+// let hours = date.getHours()
+// let min = date.getMinutes()
+// let sec = date.getSeconds()
+
+
+// let year = date.getFullYear()
+// let month = date.getMonth()+1
+// let d = new Date().getDate()
+
+//    document.getElementById('time').innerHTML =  hours +":" + min +":" + sec + "<br>"
+
+//   document.getElementById('date').innerHTML = year +"/" + month +"/" + d
+// let myDate = new Date(year , month , date , hours , min , sec)
+
+
+// let myDate = new Date(2003 , 10 , 18 , 14 , 35 ,45)
+
+// document.writeln(myDate)
+
+
+
+// let num1 = 12
+// let num2=32
+
+// document.writeln("The addition is" +"<mark>" + (num1+num2) + "</mark>"+ "The subtraction is " + (num1-num2))
+// document.writeln(`The addition is <mark> ${num1+num2} </mark> . The sub is <del> ${num1-num2} </del>  `)
+
+
+
+// let total = prompt("Enter The Total Number")
+// let obt  = prompt("Enter The Obtained Number")
+
+// let percentage = obt / total * 100
+
+// document.writeln(Math.round(percentage))
+
+// let num = 2.4 
+
+// let num1 = 2.4
+
+
+
+// document.writeln(Math.ceil(num))
+// document.writeln(Math.ceil(num) + Math.ceil(num1))
+
+
+let num = 3.9
+
+document.writeln(Math.floor(num))
